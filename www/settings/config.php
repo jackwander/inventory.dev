01631987a61df3dfb24d80b1abf28840a37797f0
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-02-04 21:17:12 +0800

    Adding something awesome.

--- www/settings/config.php
+++ www/settings/config.php
@@ -1,4 +1,13 @@
 <?php
+/* Your database connection. 
+ * Just uncomment these code to function in your computer.  
+ */
+//  define('db_server','localhost');
+//  define('DB_USERNAME','root');
+//  define('DB_PASSWORD','');
+//  define('DB_DATABASE','dbstock');
+
+/* Jerwin's database connection. */
   define('DB_SERVER','localhost');
   define('DB_USERNAME','jerwin');
   define('DB_PASSWORD','1221482');
