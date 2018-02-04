102446d1ed0fd0633a0305fe8f9f812cc3e21d1c
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-02-04 14:33:27 +0800

    Modify config settings.

--- www/settings/config.php
+++ www/settings/config.php
@@ -1,12 +1,6 @@
 <?php
-  $host = 'localhost';
-  $username = 'jerwin';
-  $password = '1221482';
-  $database = 'db_stock';
-  $dbconfig = mysqli_connect($host,$username,$password,$database);
-
   define('DB_SERVER','localhost');
   define('DB_USERNAME','jerwin');
   define('DB_PASSWORD','1221482');
-  define('DB_DATABASE','db_stock');
+  define('DB_DATABASE','dbstock');
 ?>
