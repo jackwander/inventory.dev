689080a203eaa0b50d45cdb75ecdf81e80f19700
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-02-07 03:35:37 +0800

    Edit head.php.

--- www/includes/head.php
+++ www/includes/head.php
@@ -1,5 +1,4 @@
-<head>
   <title>CRUD for inventory</title>
   <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
   <link href="../assets/plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+  <link rel="stylesheet" type="text/css" href="../assets/plugins/font-awesome/css/font-awesome.min.css">
