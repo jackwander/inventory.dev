b0715dde2688784dcddc816955951bfa23958097
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-03-17 10:42:13 +0800

    Move login.

--- www/index.php
+++ www/index.php
@@ -6,13 +6,13 @@ include('classes/class.users.php');
 $user = new User();
 
 if(!$user->get_session()){
-	header("location: login.php");
+	header("location: system/login.php");
 } else  {
   header('location: '.$_SESSION['position'].'/index.php');
 } 
 
 if(isset($_GET['q'])){
 	$user->user_logout();
-	header("location: login.php");
+	header("location: system/login.php");
 }
 ?>
