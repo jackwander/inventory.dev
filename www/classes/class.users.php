9ead073543cbcba435bf3570d05d1cf53bd7721c
Jerwin Arnado <jerwin.arnado@gmail.com>
2018-03-17 09:18:17 +0800

    Create login.

---
+++ www/classes/class.users.php
@@ -0,0 +1,37 @@
+<?php
+class User{ 
+  
+  public $db;
+
+  /* constructor para mag connect sa database. */
+  public function __construct() {
+    $this->db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+    if(mysqli_connect_errno()){
+      echo 'Error: Could not connect to Database';
+      exit;
+    }
+  } 
+  /* constructor para mag connect sa database. */
+  
+  public function check_login($user,$password) {
+    $pass = md5($password);
+    $sql = "SELECT * FROM `tbl_users` WHERE `username`='$user' AND `password`='$pass'"; 
+    $result = mysqli_query($this->db, $sql);
+		$check=$this->db->query($sql)or 
+			die(mysqli_connect_errno()."nd okay.");			
+		$count_row=$check->num_rows;
+
+    if($count_row>0){
+      while($row=mysqli_fetch_assoc($result)){
+        session_regenerate_id();
+        $_SESSION['login']=TRUE;
+        $_SESSION['user_id']=$row['user_id']; 
+        return TRUE;
+      }
+    }else{
+      return FALSE;
+    }
+  }
+
+}
+?>
---
+++ www/login.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include 'settings/config.php';
+include 'classes/class.users.php';
+$user = new User();
+
+if(isset($_REQUEST['submit'])){
+    extract($_REQUEST);
+    $login = $user->check_login($username,$password);
+    if($login){
+        echo "success";
+        header("location: index.php");
+    }else{
+        echo '<script>alert("Wrong Username or Password!")</script>';
+        }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+  <form action="" METHOD="POST">
+    <label>Username</label>
+    <input type="text" name="username" required/>
+    <label>Password</label>
+    <input type="password" name="password" required/>
+    <input type="submit" name="submit" value="Log-in" />
+  </form>
+</body>
+</html>
