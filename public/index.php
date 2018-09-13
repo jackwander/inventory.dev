817f6a9c225b8b00fb41d6e416906bab86dd3a3a
Jerwin Arnado <me@jackwander.pro>
2018-09-14 02:01:42 +0800

    Modify.

--- public/index.php
+++ public/index.php
@@ -7,14 +7,8 @@ $user = new User();
 
 if(!$user->get_session()){
 	header("location: system/login.php");
-} else {
-  if ($_SESSION['position']=='admin') {
-    header('location: admin/index.php');
-  } else if ($_SESSION['position']=='user') {
-    header('location: user/index.php');
-  } else if ($_SESSION['position']=='company') {
-    header('location: company/index.php');
-  }
+} else  {
+  header('location: '.$_SESSION['position'].'/index.php');
 } 
 
 if(isset($_GET['q'])){
